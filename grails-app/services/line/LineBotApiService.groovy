c71bd3fb0ab802bdc69d00f47c9e750fe2005030
三谷昌平 <mitanishouhei@mitanishouhei-no-MacBook.local>
2016-05-06 07:05:38 +0900

    change contennt-type

--- grails-app/services/line/LineBotApiService.groovy
+++ grails-app/services/line/LineBotApiService.groovy
@@ -15,29 +15,38 @@ class LineBotApiService {
     }
     
     def sendTextMessage(to,text){
-        def http = new HTTPBuilder( 'https://trialbot-api.line.me/v1/events' )
-        http.post( headers : ['Content-type': 'application/json; charset=UTF-8',
-                               'X-Line-ChannelID':'1466137400', //Channel ID
-                               'X-Line-ChannelSecret':'acd1150a257fee513e487da7d6ee5eaf', // Channel secre
-                               'X-Line-Trusted-User-With-ACL':' u99c2c232b6c8a2a4dc2177e6834be630' // MID (of Channel)
-                            ],
-                   body: [to: [to],
-                          toChannel: 1383378250, // Fixed value
-                          eventType: '138311608800106203', //Fixed value,
-                          content: [contentType: '1', //Text message
-                                    toType: '1',  //Type of recipient set in the to property. (1 = user)
-                                    text:text
-                                  ]
-                          ]) { resp,reader  ->
-                 
-            if (resp.status == 200) {
+        def http = new HTTPBuilder( 'https://trialbot-api.line.me' )
+        try{
+            //文字コードが不安
+            http.post( path: '/v1/events',
+                       headers : [
+                             'X-Line-ChannelID':'1466137400', //Channel ID
+                             'X-Line-ChannelSecret':'acd1150a257fee513e487da7d6ee5eaf', // Channel secre
+                             'X-Line-Trusted-User-With-ACL':' u99c2c232b6c8a2a4dc2177e6834be630' // MID (of Channel)
+                                ],
+                       body: [to: [to],
+                              toChannel: 1383378250, // Fixed value
+                              eventType: '138311608800106203', //Fixed value,
+                              content: [contentType: 1, //Text message
+                                        toType: 1,  //Type of recipient set in the to property. (1 = user)
+                                        text:text
+                                      ]
+                              ],
+                          requestContentType: JSON) { resp,reader  ->
+                
+                if (resp.status == 200) {
+                }
+                else if (resp.status == 404) {
+                    println "Access denied"
+                }
+                else {
+                    println "Unexpected failure: ${resp.statusLine}"
+                }
             }
-            else if (resp.status == 404) {
-                println "Access denied"
-            }
-            else {
-                println "Unexpected failure: ${resp.statusLine}"
-            }
+        }catch(Exception e){
+            println 'error!!!!!!!!!'
+            def res = e.getResponse()
+            println res.getStatusLine()
         }
     }
 }
